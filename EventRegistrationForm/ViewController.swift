a4c8f5b34c7257a9331ca122cd5d0cd399e2490b
Aaina Jain <aainajain100@gmail.com>
2018-07-18 22:07:22 +0530

    Initial Commit

---
+++ EventRegistrationForm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EventRegistrationForm
+//
+//  Created by Apple on 7/18/18.
+//  Copyright © 2018 aaina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
