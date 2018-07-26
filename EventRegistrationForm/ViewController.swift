512615773fc4bdfbca00255ec92bac3adc00d6d9
Aaina Jain <aainajain100@gmail.com>
2018-07-26 09:25:11 +0530

    Create event form

--- EventRegistrationForm/ViewController.swift
+++ EventRegistrationForm/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let eventForm = EventForm()
+    
+    override func loadView() {
+        super.loadView()
+        view.addSubview(eventForm)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
 
 }
 
